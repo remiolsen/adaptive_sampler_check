e0346b25b99657bc5f990379e0013dd20116f4fa
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2023-12-20 15:52:19 +0100

    First pre-release version

---
+++ README.md
@@ -0,0 +1,24 @@
+# Adaptive Sampler Check
+
+This performs a series of checks on the input BED file and selected assembly for the Oxford Nanopore adaptive sampling method. 
+Additionally, it it provides the ability to adjust the region of interest (ROI) size to be used to be used by adding a buffer to each site,
+whilst keeping keeping the total and individual ROI sizes within a recommended range.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+## Installation
+
+Instructions on how to install and set up the project.
+
+## Usage
+
+Instructions on how to use the project or any relevant examples.
+
+## Contributing
+
+Guidelines for contributing to the project.
+
--- adaptive_sampler_check.py
+++ adaptive_sampler_check.py
@@ -1,82 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import hashlib
+import time
 from io import StringIO
 
 version = "0.1.0-dev"
+program_name = "Adaptive Sampler Check"
+program_url = "https://github.com/remiolsen/adaptive_sampler_check/"
+st.set_page_config(page_title=f"{program_name} v{version}", page_icon=":scissors:", initial_sidebar_state="expanded")
 genomes = pd.DataFrame({"https://hgdownload.soe.ucsc.edu/goldenPath/hs1/bigZips/hs1.chrom.sizes.txt": "human (T2T CHM13v2.0/hs1)", 
                         "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes": "human (GRCh38/hg38)", 
                         "https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.chrom.sizes": "mouse (GRCm39/mm39)", 
                         "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.chrom.sizes": "mouse (GRCm38/mm10)",
                         "https://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.chrom.sizes": "fruit fly (BDGP6/dm6)",
-                        },
-                        index=["URL", "Assembly"]).T
+                        },index=["URL", "Assembly"]).T
 accepted_bed_types = ['object', 'int64', 'int64', 'int64', 'int64', 'object']
 minimum_ROI_size = 0.001
 maximum_ROI_size = 0.1
-
 
 @st.cache_data
 def fetch_genome(url):
     # Fetch genome size from UCSC.
     return pd.read_csv(url, sep='\t', header=None, names=["chrom", "size"])
 
-def write_bed_checks():
-    bed_results = st.container(border=True)
-    if st.session_state['bed_columns']:
-        bed_results.write(f":x: The file should have at least 3 columns, but found only `{st.session_state['bed_columns']}`")
-    else:
-        bed_results.write(":white_check_mark: The file has at least 3 columns.")
-    if st.session_state['bed_types']:
-        bed_results.write(":x: The bed values do not match the expected types:")
-        bed_results.write(accepted_bed_types)
-    else:
-        bed_results.write(":white_check_mark: The file has the correct columns types.")
-    # To be implemented
-    bed_results.write(":grey_question: The BED file entries are mostly evenly sized.")
-    bed_results.write(":grey_question: The BED does not contain any overlapping regions.")
-
-def write_assembly_checks():
-    assembly_results = st.container(border=True)
-    if st.session_state['chrom_error']:
-        assembly_results.write(f":x: The file contains the following chromosomes not present in the assembly: `{st.session_state['chrom_error']}`")
-    else:
-        assembly_results.write(":white_check_mark: All chromosomes in the file are present in the assembly.")
-    if st.session_state['size_error']:
-        assembly_results.write(f":x: The file contains the following coordinates larger than the chromosome size: `{st.session_state['size_error']}`")
-    else:
-        assembly_results.write(":white_check_mark: All coordinates in the file are smaller than the chromosome size.")
-    # To be implemented
-    assembly_results.write(":grey_question: The BED does not contain any sex chromosomes.")
-
-def modify_bed(in_bed, assembly, min_size, min_buffer):
-    bed = in_bed.copy()
-    messages = []
-    for i, row in bed.iterrows():
-        r_size = row[2] - row[1]
-        length_cutoff = assembly.loc[assembly["chrom"] == row[0], "size"].values[0]
-        missing = np.round((min_size - r_size) / 2, 0)
-        if row[1] - missing > 0 or row[2] + missing < length_cutoff:
-            bed.loc[i, 1] = row[1] - missing
-            bed.loc[i, 2] = row[2] + missing
-        elif row[1] - min_buffer > 0 and row[2] + missing*2 - min_buffer < length_cutoff:
-            bed.loc[i, 1] = 0
-            bed.loc[i, 2] = row[2] + missing*2 - row[1]
-        elif row[1] - missing*2 + min_buffer > 0 and row[2] + min_buffer < length_cutoff:
-            bed.loc[i, 1] = row[1] - missing*2 - (length_cutoff - row[2])
-            bed.loc[i, 2] = length_cutoff
-        else:
-            messages.append(f":x: Bed modification failed - new interval illegal `{row[0]} : {row[1]-missing} - {row[2]+missing}`")
-
-    if len(messages) == 0:
-        messages.append(":white_check_mark: Bed modification successful")
-    return bed, messages            
-
-
-
 # Session flow
 if 'state' not in st.session_state:
     st.session_state['state'] = 0
+    st.session_state['bed_df'] = pd.DataFrame()
     st.session_state['bed_columns'] = False
     st.session_state['bed_types'] = False
     st.session_state['assembly_df'] = pd.DataFrame()
@@ -84,7 +35,9 @@ if 'state' not in st.session_state:
     st.session_state['size_error'] = False
     st.session_state['QC_failed'] = False
     st.session_state['BED_mod_failed'] = False
-
+    st.session_state['mod_bed'] = pd.DataFrame()
+    st.session_state["metadata"] = pd.DataFrame()
+    st.session_state["out_bed"] = ""
 
 '''
 # Adaptive Sampler Check
@@ -104,39 +57,41 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     string_data = stringio.read()
-    bed_df = pd.read_csv(uploaded_file, header=None, sep='\t')
+    st.session_state['bed_df'] = pd.read_csv(uploaded_file, header=None, sep='\t')
     bed_exp = st.expander("See content of the file")
-    bed_exp.write(bed_df)
-    num_cols = bed_df.shape[1]
+    bed_exp.write(st.session_state['bed_df'])
+    num_cols = st.session_state['bed_df'].shape[1]
     if num_cols < 3:
-        st.session_state['bed_columns'] = bed_df.shape[1] 
-    res = bed_df.dtypes
+        st.session_state['bed_columns'] = st.session_state['bed_df'].shape[1]
+    res = st.session_state['bed_df'].dtypes
     if list(res[:num_cols]) != accepted_bed_types[:num_cols]:
         st.session_state['bed_types'] = res[:6].any()
-    st.session_state['state'] = 1
+    if st.session_state['state'] < 1:
+        st.session_state['state'] = 1
 
 '''
 ## 2. :dna: Choose an assembly
 '''
 
-assembly = st.selectbox("Choose and assembly", genomes, index=1, key="assembly_select", label_visibility='hidden')
-fetch_button = st.button('Fetch', disabled=st.session_state['state'] > 1, key="fetch_button")
+assembly = st.selectbox("Choose and assembly", genomes, index=1, key="assembly_select", disabled=st.session_state['state'] > 1, label_visibility='hidden')
+#fetch_button = st.button('Fetch', disabled=st.session_state['state'] > 1, key="fetch_button")
 assembly_df = pd.DataFrame()
 assembly_exp = st.expander("See content of the assembly")
-if st.session_state['state'] == 1 and fetch_button:
+#if st.session_state['state'] < 2 and fetch_button:
+if st.button('Fetch', disabled=st.session_state['state'] > 2, key="fetch_button"):
     assembly_df = fetch_genome(genomes.index[genomes["Assembly"] == assembly][0])
-    assembly_exp.write(assembly_df)
-
-    for chrom in bed_df[0].unique():
-        if chrom not in assembly_df["chrom"].values:
+    st.session_state['assembly_df'] = assembly_df
+    st.session_state['state'] = 2
+if st.session_state['state'] < 3 and not st.session_state['assembly_df'].empty:
+    assembly_exp.write(st.session_state['assembly_df'])
+    for chrom in st.session_state['bed_df'][0].unique():
+        if chrom not in st.session_state['assembly_df']["chrom"].values:
             st.session_state['chrom_error'] = chrom
-    for i, row in bed_df.iterrows():
-        if not st.session_state['chrom_error'] and row[2] > assembly_df.loc[assembly_df["chrom"] == row[0], "size"].values[0]:
+    for i, row in st.session_state['bed_df'].iterrows():
+        if not st.session_state['chrom_error'] and row[2] > st.session_state['assembly_df'].loc[st.session_state['assembly_df']["chrom"] == row[0], "size"].values[0]:
             st.session_state['size_error'] = row[1]
         elif st.session_state['chrom_error']:
             st.session_state['size_error'] = "Not found"
-    st.session_state['assembly_df'] = assembly_df
-    st.session_state['state'] = 2
 
 '''
 ## 3. :sleuth_or_spy: QC results
@@ -144,39 +99,165 @@ if st.session_state['state'] == 1 and fetch_button:
 
 if st.session_state['state'] > 1:
     ''' :page_facing_up: BED file checks '''
-    write_bed_checks()
+    bed_results = st.container(border=True)
+    if st.session_state['bed_columns']:
+        bed_results.write(f":x: The file should have at least 3 columns, but found only `{st.session_state['bed_columns']}`")
+    else:
+        bed_results.write(":white_check_mark: The file has at least 3 columns.")
+    if st.session_state['bed_types']:
+        bed_results.write(":x: The bed values do not match the expected types:")
+        bed_results.write(accepted_bed_types)
+    else:
+        bed_results.write(":white_check_mark: The file has the correct columns types.")
+    # To be implemented
+    bed_results.write(":grey_question: The BED file entries are mostly evenly sized.")
+    bed_results.write(":grey_question: The BED does not contain any overlapping regions.")
+    bed_results.write(":grey_question: Start coord (col 2) is larger than end coord (col 3)")
+
     ''' :dna: Assembly checks '''
-    write_assembly_checks()
-    st.session_state['state'] = 3
+    assembly_results = st.container(border=True)
+    if st.session_state['chrom_error']:
+        assembly_results.write(f":x: The file contains the following chromosomes not present in the assembly: `{st.session_state['chrom_error']}`")
+    else:
+        assembly_results.write(":white_check_mark: All chromosomes in the file are present in the assembly.")
+    if st.session_state['size_error']:
+        assembly_results.write(f":x: The file contains the following coordinates larger than the chromosome size: `{st.session_state['size_error']}`")
+    else:
+        assembly_results.write(":white_check_mark: All coordinates in the file are smaller than the chromosome size.")
+    # To be implemented
+    assembly_results.write(":grey_question: The BED does not contain any sex chromosomes.")
 
 '''
 ## 4. :gear: Parameters
 '''
-    
-experiment_name = st.text_input("Experiment name", key="experiment_name", label_visibility='hidden', max_chars=100, placeholder="adaptive_sampling_01")
-''' Experiment name '''
-operator_name = st.text_input("Operator name", key="operator_name", label_visibility='hidden', max_chars=100, placeholder="N. Hvermannsen")
-''' Operator name '''
-minimum_buffer_size = st.number_input("Minimum buffer size (bp)", min_value=10000, max_value=10000000, key="min_buffer_size", value=40000, step=10000, format="%d", label_visibility='hidden')
-''' Minimum buffer size (bp) '''
-roi_size = st.slider("ROI size", min_value=minimum_ROI_size, max_value=maximum_ROI_size, key="ROI_slider",value=0.001, step=0.001, format="%.3f", label_visibility='hidden')
-''' ROI size '''
-size_override = st.toggle("Override size limitations", key="size_override", value=False, label_visibility='hidden', disabled=True)
-''' Override size limitations '''
-chrom_prune = st.toggle("Prune missing chromosomes", key="chrom_prune", value=False, label_visibility='hidden', disabled=True)
-''' Prune missing chromosomes '''
+
+col1, col2 = st.columns(2)
+
+with col1:
+    experiment_name = st.text_input("Experiment name", key="experiment_name", label_visibility='hidden', max_chars=100, placeholder="adaptive_sampling_01")
+    ''' Experiment name '''
+    minimum_buffer_size = st.number_input("Minimum buffer size (bp)", min_value=10000, max_value=10000000, key="min_buffer_size", value=40000, step=10000, format="%d", label_visibility='hidden')
+    ''' Minimum buffer size (bp) '''
+with col2:
+    operator_name = st.text_input("Operator name", key="operator_name", label_visibility='hidden', max_chars=100, placeholder="N. Hvermannsen")
+    ''' Operator name '''
+    roi_size = st.slider("ROI size", min_value=minimum_ROI_size, max_value=maximum_ROI_size, key="ROI_slider",value=0.001, step=0.001, format="%.3f", label_visibility='hidden')
+    ''' ROI size '''
+
+scol1, scol2, scol3 = st.columns(3)
+with scol1:
+    size_override = st.toggle("Override size limitations", key="size_override", value=False, label_visibility='hidden', disabled=True)
+    ''' Override size limitations '''
+with scol2:
+    chrom_prune = st.toggle("Prune missing chroms", key="chrom_prune", value=False, label_visibility='hidden', disabled=True)
+    ''' Prune missing chroms '''
+with scol3:
+    no_sort = st.toggle("No sorting", key="no_sort", value=False, label_visibility='hidden', disabled=True)
+    ''' No sorting '''
+
+@st.cache_data
+def modify_bed(bed_df, assembly_df, roi_size, min_size, minimum_buffer_size):
+    mod_bed = bed_df.copy()
+    messages = []
+    bad_bed = False
+    for i, row in mod_bed.iterrows():
+        r_size = row[2] - row[1]
+        length_cutoff = assembly_df.loc[assembly_df["chrom"] == row[0], "size"].values[0]
+        missing = np.round((min_size - r_size) / 2, 0)
+        if row[1] - missing > 0 and row[2] + missing < length_cutoff:
+            mod_bed.loc[i, 1] = row[1] - missing
+            mod_bed.loc[i, 2] = row[2] + missing
+        elif row[1] - minimum_buffer_size > 0 and row[2] + missing*2 - minimum_buffer_size < length_cutoff:
+            mod_bed.loc[i, 1] = 0
+            mod_bed.loc[i, 2] = row[2] + missing*2 - row[1]
+        elif row[1] - missing*2 + minimum_buffer_size > 0 and row[2] + minimum_buffer_size < length_cutoff:
+            mod_bed.loc[i, 1] = row[1] - missing*2 - (length_cutoff - row[2])
+            mod_bed.loc[i, 2] = length_cutoff
+        else:
+            messages.append(f":x: Bed modification failed - new interval illegal `{row[0]} : {row[1]-missing} - {row[2]+missing}`")
+            bad_bed = True
+    return bad_bed, mod_bed, messages
 
 if st.button('Generate', disabled=st.session_state['state'] < 2, key="generate_button"):
+    bad_bed = False
     genome_size = st.session_state['assembly_df']["size"].sum()
-    modded_bed, messages = modify_bed(bed_df, st.session_state['assembly_df'], np.round(roi_size * genome_size, 0), minimum_buffer_size)
+    roi_size = st.session_state['ROI_slider']
+    bad_bed, mod_bed, messages = modify_bed(st.session_state['bed_df'], st.session_state['assembly_df'], roi_size, np.round(roi_size * genome_size, 0), minimum_buffer_size)
+    if not st.session_state['no_sort']:
+        mod_bed = mod_bed.sort_values(by=[0,1])
+    total_fraction = (mod_bed[2].sum() - mod_bed[1].sum()) / genome_size
+    if total_fraction > maximum_ROI_size and not st.session_state['size_override']:
+        messages.append(f":x: Total ROI size is larger than the recommended maximum of `{maximum_ROI_size}`")
+        bad_bed = True
+    if len(messages) == 0 and not bad_bed:
+        messages.append(":white_check_mark: Bed modification successful")
+    total_size = mod_bed[2].sum() - mod_bed[1].sum() / 1000000
+    median_size = (mod_bed[2] - mod_bed[1]).median() / 1000000
+    messages.append(f":information_source: total size: `{int(np.round(total_size))} mbp` (`{np.round(total_fraction * 100,2)} %` of genome), median size: `{np.round(median_size,2)} mbp`")
     msg_container = st.container(border=True)
     for msg in messages:
         msg_container.write(msg)
     mod_exp = st.expander("See modified BED")
-    mod_exp.write(modded_bed)
+    mod_exp.write(mod_bed)
+    st.session_state['mod_bed'] = mod_bed
+    if not st.session_state['operator_name']:
+        st.error(":exclamation: Operator name is required")
+        bad_bed = True
+    if not st.session_state['experiment_name']:
+        st.error(":exclamation: Experiment name is required")
+        bad_bed = True
+
+    if not bad_bed:
+        st.session_state['state'] = 4
+    else:
+        st.session_state['state'] = 3
 
 
 '''
 ## 5. :arrow_down: Download 
 '''
 
+mod_bed_name = f"{experiment_name}.bed"
+local_time = time.localtime()
+tzname_local = local_time.tm_zone
+if st.session_state['state'] > 3:
+    st.session_state["out_bed"] = st.session_state['mod_bed'].to_csv(index=False, sep="\t", header=False)
+    st.session_state["metadata"] = pd.DataFrame({"key":[
+                                        "operator",
+                                        "experiment",
+                                        "assembly",
+                                        "assembly_url",
+                                        "input_bed",
+                                        "input_bed_md5",
+                                        "download_bed",
+                                        "download_bed_md5",
+                                        "date_generated",
+                                        "version",
+                                        "program_url"
+                                ],
+                                "value": [
+                                        st.session_state['operator_name'],
+                                        st.session_state['experiment_name'],
+                                        st.session_state['assembly_select'],
+                                        genomes.index[genomes["Assembly"] == st.session_state['assembly_select']][0],
+                                        uploaded_file.name,
+                                        hashlib.md5(bytes_data).hexdigest(),
+                                        mod_bed_name,
+                                        hashlib.md5(st.session_state["out_bed"].encode('utf-8')).hexdigest(),
+                                        pd.Timestamp.now(tz=tzname_local).isoformat(timespec='seconds'),
+                                        version,
+                                        program_url
+                                        ]
+                                })
+    st.table(st.session_state["metadata"])
+metadata_name = f"{st.session_state['experiment_name']}_metadata.csv"
+bcol1, bcol2 = st.columns(2)
+with bcol1:
+    st.download_button('Download BED', st.session_state["out_bed"], mod_bed_name, 'text/plain', key="dl_bed", disabled=st.session_state['state'] < 4)
+with bcol2:
+    st.download_button('Download Metadata', st.session_state["metadata"].to_csv(index=False), metadata_name, 'text/plain', key="dl_metadata", disabled=st.session_state['state'] < 4)
+
+'''
+---
+'''
+st.markdown(f"Created using {program_name} v{version} - [GitHub]({program_url})")
